9341f3bf7ef2d926f54b4a6de5efc5d8bf8ee9d6
samade747 <samad.e747@gmail.com>
2023-09-18 23:00:18 +0500

    update coding1

---
+++ app.js
@@ -0,0 +1,49 @@
+// creating To Do App 
+
+
+
+var getul = document.getElementById('ul')
+
+function foo1(){
+    var ca = document.getElementById('inp2')
+    var li = document.createElement('li')
+    var litext= document.createTextNode(ca.value)
+    li.appendChild(litext)
+    getul.appendChild(li)
+    ca.value = ''
+    var deletebtn = document.createElement('button');
+    var deletebtnText = document.createTextNode("Delete");
+    deletebtn.appendChild(deletebtnText);
+    li.appendChild(deletebtn);
+    deletebtn.setAttribute('onclick', 'del(this.parentNode)');
+    var editBtn = document.createElement('button');
+    var editBtnText = document.createTextNode('Edit');
+    editBtn.appendChild(editBtnText)
+    li.appendChild(editBtn)
+    editBtn.setAttribute('onclick','editfun(this)' )
+    deletebtn.setAttribute('class', 'btn btn-danger');
+    editBtn.setAttribute('class', 'btn btn-info aaa')
+}
+
+function deleteall(){
+    getul.innerHTML = ''
+}
+
+  
+
+function del(e){
+   e.parentNode.remove()
+
+}
+
+
+function editfun(e){
+    var a = prompt('Enter edit value', e.parentNode.firstChild.nodeValue);
+    if (a !== null) {
+        e.parentNode.firstChild.nodeValue = a;
+    }
+}
+
+
+
+
